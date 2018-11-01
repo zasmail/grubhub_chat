4f5a6d23c448970b702a4d809298e5219b37939b
Peter Ekene <ekeneeze3@gmail.com>
2018-11-01 14:13:44 +0100

    Updated server.js
    Removed credentials

--- server.js
+++ server.js
@@ -7,8 +7,8 @@
     
     // init chatkit
     const chatkit = new Chatkit.default({
-      instanceLocator: 'v1:us1:7db04fbc-6070-4c06-bc58-a702f0d72695',
-      key: '24460b86-67e4-446d-9d01-0ed796ad9a01:mhhKezpl9SMKm8lrBTHsIPg601OjOGCyE0XsMI4uMxM=',
+      instanceLocator: 'YOUR_INSTANCE_LOCATOR',
+      key: 'YOUR_SECRET_KEY',
     })
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(bodyParser.json())
